c955138a8b7d3d81d6670e39e577e09e22bafc8b
Ruwayne <137304473+Humblefrib@users.noreply.github.com>
2025-01-12 20:17:39 +0200

    Update introTypeScript.js

--- introTypeScript.js
+++ introTypeScript.js
@@ -1,10 +1,10 @@
 var typed = new Typed(".auto-type", {
     strings: [
-        "a front-end web developer...",
+        "a front-end developer...",
         "a programmer...",
-        "a graphic designer",
+        "a graphic designer...",
         "a freelancer...",
-        "UI/UX designer...",
+        "UI / UX designer...",
         ],
     typeSpeed: 100,
     backSpeed: 50,
